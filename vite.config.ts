ec71eb8712ebf8d940fad164bdacfe750ea5b94b
Yoshida28 <nitishsaravanan63@gmail.com>
2025-11-27 20:11:49 +0530

    removed gemini apikey

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const __dirname = path.resolve(); // Added to define __dirname
+const __dirname = path.resolve();
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.API_KEY) // Removed GEMINI_API_KEY
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
